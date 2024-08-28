8be9b7bf5fccf9ddd0346b9a3609600e999d3efb
Vinod28joc <vinodjoshi2894@gmail.com>
2024-08-28 11:23:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title('👽 Digi Hack Service')
 
 st.info('This service is build to summarise the customer intraction.')
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     text_input = st.text_input(
@@ -17,3 +17,5 @@ with col2:
         "Enter Email address 👇",
         placeholder="eg:brian@gmail.com",
     )
+ with col3:   
+ d = st.date_input("When's your birthday", datetime..today())   
