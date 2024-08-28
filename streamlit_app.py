58b6834dcfdbe82ffe44acb4a394b5e4504c3017
Vinod28joc <vinodjoshi2894@gmail.com>
2024-08-28 14:24:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ st.info('This service is build to summarise the customer intraction.')
 col1, col2 = st.columns(2)
 
 with col1:
-    text_input = st.text_input(
+    text_input1 = st.text_input(
         "Enter Customer id 👇",
         placeholder="eg:12345",
     )
 
 with col2:
-    text_input = st.text_input(
+    text_input2 = st.text_input(
         "Enter Email address 👇",
         placeholder="eg:brian@gmail.com",
     )
@@ -23,6 +23,12 @@ d = st.date_input("When's your birthday", value=None)
 st.button("Generate Summary") 
 st.button("Send email") 
 st.button("Reset", type="primary")
+
+with st.form("my_form"):
+    submitted = st.form_submit_button("Generate Summary")
+    if submitted:
+        st.write("first input", text_input1, "second Input", text_input2)
+
 
 
 
