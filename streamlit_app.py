c89e09989552074b3a1e656b2b567ecbeb7bd069
Vinod28joc <vinodjoshi2894@gmail.com>
2024-08-28 11:19:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.title('👽 Digi Hack Service')
 
 st.info('This service is build to summarise the customer intraction.')
+
+with col1:
+    text_input = st.text_input(
+        "Enter Customer id 👇",
+        placeholder="eg:12345",
+    )
+
+with col2:
+    text_input = st.text_input(
+        "Enter Email address 👇",
+        placeholder="eg:brian@gmail.com",
+    )
