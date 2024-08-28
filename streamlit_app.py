b10b2e13cca9ce7035eee9671b327567cd59a2f8
Vinod28joc <vinodjoshi2894@gmail.com>
2024-08-28 10:58:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('👽 Digi Hack Service')
 
-st.write('This service is build to summarise the customer intraction.')
+st.info('This service is build to summarise the customer intraction.')
